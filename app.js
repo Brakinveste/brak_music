e90607a2332acfa660e585e206e0ae835e0db54e
Brakinveste <brakinveste@gmail.com>
2022-10-12 11:52:00 -0300

    commit 5  brakmusic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let now_playing = document.querySelector('.now-playing');
-let track_art = document.querySelector('.track-art');
+
+let track_art = document.querySelector('.pic');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
 
@@ -11,7 +11,7 @@ let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
+//let wave = document.getElementById('wave');
 let randomIcon = document.querySelector('.fa-random');
 let curr_track = document.createElement('audio');
 
@@ -22,46 +22,76 @@ let updateTimer;
 
 const music_list = [
     {
-        img : './img/001.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
+        img : './img/yoda.jpg',
+        name : 'BRAK MUSIC',
+        artist : '',
+        music : './music/yoda.mp3'
+    },
+    {
+        img : './img/aint_no_sunshine-canen.jpg',
+        name : 'Aint No Sunshine',
+        artist : 'Canen',
+        music : './music/aint_no_sunshine-canen.mp3'
+    },
+    {
+        img : './img/dont_say_goodbye-alok.jpg',
+        name : "Don't Say Goodbye",
+        artist : 'Alok',
+        music : './music/dont_say_goodbye-alok.mp3'
+    },
+    {
+        img : './img/duckTales-saturday_morning_slow_jams.jpg',
+        name : 'DuckTales',
+        artist : 'Saturday Morning Slow Jams',
+        music : './music/duckTales-saturday_morning_slow_jams.mp3'
+    },
+    {
+        img : './img/gangstas_paradise-lv.jpg',
+        name : "Gangsta's Paradise",
+        artist : 'L.V.',
+        music : './music/gangstas_paradise-lv.mp3'
+    },
+    {
+        img : './img/bad_to_the_bone-George_thorogood.jpg',
+        name : 'Bad to the Bone',
+        artist : 'George Thorogood',
+        music : './music/bad_to_the_bone-George_thorogood.mp3'
+    },
+    {
+        img : './img/001.jpg',
+        name : 'Itsy Bitsy Spider',
+        artist : 'Joey Deluxe',
         music : './music/001.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/002.mp3'
+        img : './img/002.jpg',
+        name : 'River',
+        artist : 'Bishop Briggs',
+        music : './music/002.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/003.mp3'
+        img : './img/003.jpg',
+        name : 'The Humbling River',
+        artist : 'Puscifer',
+        music : './music/003.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/004.mp3'
+        img : './img/004.jpg',
+        name : 'This Life',
+        artist : 'Curtis Stigers & The Forest Ranger',
+        music : './music/004.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/005.mp3'
+        img : './img/005.jpg',
+        name : 'Wicked Game',
+        artist : 'Ursine Vulpine & Annaca',
+        music : './music/005.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/006.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/007.mp3'
+        img : './img/006.jpg',
+        name : 'Velho Opalão',
+        artist : 'Garagem Bluseira',
+        music : './music/006.mp3'
     }
 ];
 
@@ -81,7 +111,7 @@ function loadTrack(track_index){
 
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    
 
     updateTimer = setInterval(setUpdate, 1000);
 
@@ -103,9 +133,9 @@ function random_bg_color(){
     }
     let Color1 = populate('#');
     let Color2 = populate('#');
-    var angle = 'to right';
+    var angle = 'to left';
 
-    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ',' + Color1 + ")";
     document.body.style.background = gradient;
 }
 function reset(){
@@ -130,22 +160,32 @@ function repeatTrack(){
     playTrack();
 }
 function playpauseTrack(){
+    
     isPlaying ? pauseTrack() : playTrack();
 }
 function playTrack(){
     curr_track.play();
     isPlaying = true;
-    track_art.classList.add('rotate');
-    wave.classList.add('loader');
+   // track_art.classList.add('rotate');
+    //wave.classList.add('loader');
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 function pauseTrack(){
     curr_track.pause();
     isPlaying = false;
-    track_art.classList.remove('rotate');
-    wave.classList.remove('loader');
+    //track_art.classList.remove('rotate');
+   // wave.classList.remove('loader');
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
+
+/*
+function removeFistTrack() {
+    music_list.shift()
+} 
+*/
+
+
+
 function nextTrack(){
     if(track_index < music_list.length - 1 && isRandom === false){
         track_index += 1;
@@ -157,6 +197,9 @@ function nextTrack(){
     }
     loadTrack(track_index);
     playTrack();
+    //removeFistTrack()
+       
+
 }
 function prevTrack(){
     if(track_index > 0){
@@ -194,3 +237,4 @@ function setUpdate(){
         total_duration.textContent = durationMinutes + ":" + durationMinutes;
     }
 }
+playRandom()
